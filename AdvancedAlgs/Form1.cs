d1833269b51bde5903096bb0c6967011d332d860
Ferenc Grecu <mivor20@gmail.com>
2014-03-03 10:41:18 +0200

    Working graphics test

--- AdvancedAlgs/Form1.cs
+++ AdvancedAlgs/Form1.cs
@@ -12,9 +12,34 @@ namespace AdvancedAlgs
 {
     public partial class Form1 : Form
     {
+        private Bitmap image;
+
         public Form1()
         {
             InitializeComponent();
+            this.Width = 1200;
+            this.Height = 620;
+            pbFrame.Width = this.Width;
+            pbFrame.Height = this.Height;
+
+            image = new Bitmap(pbFrame.Width, pbFrame.Height);
+            using (Graphics gx = Graphics.FromImage(image))
+            {
+                // uncomment for higher quality output
+                gx.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                gx.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                gx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                // test
+                gx.DrawEllipse(new Pen(Color.Red), 10, 10, 333, 333);
+                gx.DrawRectangle(new Pen(Color.Green), 50, 50, 200, 200);
+            }
+
+        }
+
+        private void pbFrame_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(image, 0, 0);
         }
     }
 }
