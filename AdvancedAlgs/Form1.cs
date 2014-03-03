f0dbe654be70107e6ffe1c683f96478963b561f8
Ferenc Grecu <mivor20@gmail.com>
2014-03-03 11:01:14 +0200

    Workng rectangle drawing

--- AdvancedAlgs/Form1.cs
+++ AdvancedAlgs/Form1.cs
@@ -17,8 +17,8 @@ namespace AdvancedAlgs
         public Form1()
         {
             InitializeComponent();
-            this.Width = 1200;
-            this.Height = 620;
+            this.Width = 500;
+            this.Height = 550;
             pbFrame.Width = this.Width;
             pbFrame.Height = this.Height;
 
@@ -35,6 +35,26 @@ namespace AdvancedAlgs
                 gx.DrawRectangle(new Pen(Color.Green), 50, 50, 200, 200);
             }
 
+            Point startPos = new Point(10, 10);
+            Size startSize = new Size(450, 450);
+            clearImage();
+            drawRect(startPos, startSize);
+        }
+
+        private void clearImage()
+        {
+            using (Graphics gx = Graphics.FromImage(image))
+            {
+                gx.Clear(Color.White);
+            }
+        }
+
+        private void drawRect(Point pos, Size size)
+        {
+            using (Graphics gx = Graphics.FromImage(image))
+            {
+                gx.FillRectangle(new SolidBrush(Color.Plum), pos.X, pos.Y, size.Width, size.Height);
+            }
         }
 
         private void pbFrame_Paint(object sender, PaintEventArgs e)
