b196091f3b7545b6e586ae60196359fb2cb6b0f2
Ferenc Grecu <mivor20@gmail.com>
2014-06-13 20:37:48 +0300

    Add number factorial and tests

---
+++ Algs.RecursiveTests/FactorialTest.cs
@@ -0,0 +1,36 @@
+﻿using Algs.Recursive;
+using Xunit;
+
+namespace Algs.RecursiveTests
+{
+    public class FactorialTest
+    {
+        private Factorial _sut;
+
+        [Fact]
+        public void WhenZero_ReturnOne()
+        {
+            // Arrange
+            _sut = new Factorial(0);
+            
+            // Act
+            _sut.Execute();
+
+            // Assert
+            Assert.Equal(1, _sut.Result);
+        }
+        
+        [Fact]
+        public void WhenPositiveNumber_ReturnFactorial()
+        {
+            // Arrange
+            _sut = new Factorial(5);
+            
+            // Act
+            _sut.Execute();
+
+            // Assert
+            Assert.Equal(120, _sut.Result);
+        }
+    }
+}
