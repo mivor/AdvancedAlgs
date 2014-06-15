699d35bcce1d5b14629b2eb5bc785bc3fbf6c211
Ferenc Grecu <mivor20@gmail.com>
2014-06-16 02:43:52 +0300

    Add custom control & remove MVVM Light

--- Fractal/App.xaml.cs
+++ Fractal/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
-using GalaSoft.MvvmLight.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace Fractal
 {
@@ -8,9 +13,5 @@ namespace Fractal
     /// </summary>
     public partial class App : Application
     {
-        static App()
-        {
-            DispatcherHelper.Initialize();
-        }
     }
 }
---
+++ Fractal/CustomControls/Fractal.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Fractal.CustomControls
+{
+    public class Fractal : Canvas
+    {
+        private readonly int[] _transform = { 0, 1, 2 };
+
+        public static readonly DependencyProperty MarginDividerProperty = DependencyProperty.Register(
+            "MarginDivider", typeof(int), typeof(Fractal), new PropertyMetadata(default(int), PropertyChangedCallback));
+        public static readonly DependencyProperty MinLengthProperty = DependencyProperty.Register(
+            "MinLength", typeof(int), typeof(Fractal), new PropertyMetadata(default(int), PropertyChangedCallback));
+        public static readonly DependencyProperty RectangleBrushProperty = DependencyProperty.Register(
+            "RectangleBrush", typeof(Brush), typeof(Fractal), new PropertyMetadata(default(Brush), PropertyChangedCallback));
+
+        public int MarginDivider
+        {
+            get { return (int)GetValue(MarginDividerProperty); }
+            set { SetValue(MarginDividerProperty, value); }
+        }
+
+        public int MinLength
+        {
+            get { return (int)GetValue(MinLengthProperty); }
+            set { SetValue(MinLengthProperty, value); }
+        }
+
+        public Brush RectangleBrush
+        {
+            get { return (Brush)GetValue(RectangleBrushProperty); }
+            set { SetValue(RectangleBrushProperty, value); }
+        }
+
+
+        public Fractal()
+        {
+            Draw(new Point(0, 0), Math.Min(ActualWidth, ActualHeight));
+
+            SizeChanged += OnPropertyChanged;
+        }
+
+        private void OnPropertyChanged(object sender, EventArgs eventArgs)
+        {
+            Children.Clear();
+            Draw(new Point(0, 0), Math.Min(ActualWidth, ActualHeight));
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var fractral = (Fractal)dependencyObject;
+            fractral.OnPropertyChanged(fractral, new EventArgs());
+        }
+
+        private void Draw(Point origin, double length)
+        {
+            if (length <= MinLength)
+            {
+                AddRectangle(origin, length);
+                return;
+            }
+
+            length /= 3;
+            var margin = GetMargin(length);
+            var rectLength = length - margin * 2;
+
+            for (var i = 0; i < _transform.Length; i++)
+            {
+                for (var j = 0; j < _transform.Length; j++)
+                {
+                    // don't draw in rectangle center
+                    if (i == 1 && j == 1) continue;
+
+                    var offset = new Point
+                    {
+                        X = origin.X + margin + length * _transform[i],
+                        Y = origin.Y + margin + length * _transform[j]
+                    };
+                    Draw(offset, rectLength);
+                }
+            }
+        }
+
+        private void AddRectangle(Point offset, double length)
+        {
+            var rect = new Rectangle
+            {
+                Stroke = RectangleBrush,
+                Width = length,
+                Height = length,
+                RenderTransform = new TranslateTransform(offset.X, offset.Y)
+            };
+
+            Children.Add(rect);
+        }
+
+        private double GetMargin(double length)
+        {
+            return length / MarginDivider;
+        }
+    }
+}
