524ef1fb647a65b3f1764375a5274b1ca8fc0906
Ferenc Grecu <mivor20@gmail.com>
2014-06-16 01:56:26 +0300

    Add fractal

---
+++ Fractal/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using Fractal.Model;
+
+namespace Fractal.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
