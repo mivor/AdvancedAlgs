be2dba1ff6fa0a23d468db954a816fb5b6150372
Ferenc Grecu <mivor20@gmail.com>
2014-06-30 09:23:52 +0300

    Add greedy example

---
+++ Algs.Recursive/Greedy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algs.Simple
+{
+    public static class Greedy
+    {
+        public static int Spectacole(List<Tuple<int, int>> spectacole)
+        {
+            var sortedSpect = spectacole.OrderBy(sp => sp.Item2).ToList();
+            var result = 1;
+            var lastEndTime = spectacole.First().Item2;
+
+            for (var i = 1; i < sortedSpect.Count; i++)
+            {
+                var curentSpect = sortedSpect[i];
+                if (curentSpect.Item1 <= lastEndTime) continue;
+
+                result++;
+                lastEndTime = curentSpect.Item2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algs.RecursiveTests/GreedyTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using Algs.Simple;
+using Xunit;
+
+namespace Algs.RecursiveTests
+{
+    public class GreedyTests
+    {
+        [Fact]
+        public void Spectactole_Corect()
+        {
+
+            // Arrange
+            var spect = new List<Tuple<int, int>>
+            {
+                new Tuple<int, int>(1, 4),
+                new Tuple<int, int>(4, 7),
+                new Tuple<int, int>(3, 5),
+                new Tuple<int, int>(8, 9),
+                new Tuple<int, int>(6, 7),
+            };
+
+            // Act
+            var result = Greedy.Spectacole(spect);
+
+            // Assert
+            Assert.Equal(3, result);
+
+        } 
+    }
+}
